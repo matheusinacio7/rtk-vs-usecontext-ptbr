231da4e7b87f208a807b8849f66fc8621565d806
heyset <c.inaciomatheus@gmail.com>
2021-07-22 17:02:17 -0300

    Adiciona hooks

---
+++ src/hooks/useUser.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState } from 'react';
+
+const UserContext = createContext({});
+
+export function UserProvider({ children }) {
+  const [name, setName] = useState('');
+  const [githubUsername, setGithubUsername] = useState('');
+  const [highestCount, setHighestCount] = useState(0);
+
+  // wrappers para poder adicionar funcionalidade caso necessario sem modificar a interface;
+  const updateName = setName;
+  const updateGithubUsername = setGithubUsername;
+
+  function updateHighestCount(count) {
+    if (count > highestCount) {
+      setHighestCount(count);
+    }
+  }
+
+  return (
+    <UserContext.Provider
+      value={{
+        name,
+        githubUsername,
+        highestCount,
+        updateName,
+        updateGithubUsername,
+        updateHighestCount,
+      }}
+    >
+      { children }
+    </UserContext.Provider>
+  )
+}
+
+const useUser = () => useContext(UserContext);
+
+export default useUser;
