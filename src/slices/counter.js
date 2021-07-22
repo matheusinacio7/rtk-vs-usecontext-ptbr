9a595c6bbcda0d561a1a4be5f957ea5ffe248a1d
heyset <c.inaciomatheus@gmail.com>
2021-07-22 16:48:42 -0300

    Isola reducers de counter

--- src/slices/counter.js
+++ src/slices/counter.js
@@ -4,22 +4,24 @@ const initialState = {
   value: 0,
 };
 
+const reducers = {
+  increment: (state) => {
+    state.value += 1;
+  },
+
+  decrement: (state) => {
+    state.value -= 1;
+  },
+
+  incrementByAmount: (state, action) => {
+    state.value += action.payload;
+  },
+};
+
 export const counterSlice = createSlice({
   name: 'counter',
   initialState,
-  reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-
-    decrement: (state) => {
-      state.value -= 1;
-    },
-
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
-    },
-  },
+  reducers,
 });
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
